19dc18914e3280cd81a0157d809e989a15e2eeb5
kingsley <kessy06@yahoo.com>
2025-10-02 22:57:39 +0100

    Bulk update: Added multiple files to switch to green environment

--- variables.tf
+++ variables.tf
@@ -9,14 +9,3 @@ variable "account_id" {
   type        = string
   default     = "647540925028"
 }
-   ###
-variable "active_environment" {
-  description = "Which environment should receive traffic (blue or green)"
-  type        = string
-  default     = "blue"
-  validation {
-    condition     = contains(["blue", "green"], var.active_environment)
-    error_message = "Active environment must be either 'blue' or 'green'."
-  }
-}
-   ###
